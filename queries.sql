6b506cbd84aad042e386ea680657098b6a10f774
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-19 21:54:56 -0500

    [Add] fixed sql queries. used wrong data base for data. All queries updated to northwind db.

--- queries.sql
+++ queries.sql
@@ -2,22 +2,22 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
-SELECT ProductName, Categories.categoryname FROM Products
-JOIN Categories ON Products.categoryid = categories.categoryid
+SELECT ProductName, Category.categoryname FROM Product
+JOIN Category ON Product.categoryid = category.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-SELECT orders.orderid, orderdetails.orderid, shippers.shippername FROM orders
-INNER JOIN orderdetails ON orders.orderid = orderdetails.orderid
-INNER JOIN Shippers on orders.shipperid = shippers.shipperid
-WHERE orders.orderdate < "2012-08-09"
-ORDER BY orderdate desc;
+SELECT id, shipper.companyname FROM [order] as o
+INNER JOIN shipper ON o.shipvia = shipper.id
+WHERE o.orderdate < "2012-08-09"
+ORDER BY o.orderdate desc;
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-SELECT orderid, orderdetails.productID, products.productname FROM orderdetails
-JOIN products ON orderdetails.productid = products.productid
+SELECT product.productname, orderdetail.quantity From orderdetail
+JOIN product ON orderdetail.productid = product.id
 WHERE orderid = 10251;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
-SELECT orders.orderid, customers.customername, employees.lastname FROM orders
-INNER JOIN customers ON orders.customerid = customers.customerid
-INNER JOIN employees ON orders.employeeid = employees.employeeid;
+SELECT o.id, customer.companyname, employee.lastname FROM [order] as o
+INNER JOIN customer ON o.customerid = customer.id
+INNER JOIN employee ON o.employeeid = employee.id;
